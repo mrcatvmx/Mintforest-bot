498330625abd529f8c8c6078b9c94e41465e1974
Cipher_Airdrop <138387616+muzammilvmx@users.noreply.github.com>
2024-07-06 15:19:31 +0430

    Update api.js

--- api.js
+++ api.js
@@ -1,38 +1,76 @@
+// Required dependencies
 const axios = require('axios');
 const fs = require('fs');
 
+// Function to fetch energy list from the API
 const fetchEnergy = async (token) => {
   try {
     const response = await axios.get(
       'https://www.mintchain.io/api/tree/energy-list',
       {
         headers: {
-          Authorization: token,
+          Accept: 'application/json, text/plain, */*',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Accept-Language': 'en-US,en;q=0.7',
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          Priority: 'u=1, i',
+          Referer: 'https://www.mintchain.io/mint-forest',
+          'Sec-Ch-Ua':
+            '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+          'Sec-Ch-Ua-Mobile': '?0',
+          'Sec-Ch-Ua-Platform': '"macOS"',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Gpc': '1',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+          Authorization: token, // Authorization header with token
         },
       }
     );
-    return response.data;
+    return response.data; // Return the fetched data
   } catch (error) {
-    throw error;
+    throw error; // Throw error if fetching fails
   }
 };
 
+// Function to fetch user information from the API
 const fetchUserInfo = async (token) => {
   try {
     const response = await axios.get(
       'https://www.mintchain.io/api/tree/user-info',
       {
         headers: {
-          Authorization: token,
+          Accept: 'application/json, text/plain, */*',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Accept-Language': 'en-US,en;q=0.7',
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          Priority: 'u=1, i',
+          Referer: 'https://www.mintchain.io/mint-forest',
+          'Sec-Ch-Ua':
+            '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+          'Sec-Ch-Ua-Mobile': '?0',
+          'Sec-Ch-Ua-Platform': '"macOS"',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Gpc': '1',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+          Authorization: token, // Authorization header with token
         },
       }
     );
-    return response.data;
+    return response.data; // Return the fetched data
   } catch (error) {
-    console.log(error);
+    console.log(error); // Log error if fetching fails
   }
 };
 
+// Function to claim energy from the API
 const claimEnergy = async (token, uid, amount, includes, type, id) => {
   try {
     const response = await axios.post(
@@ -47,16 +85,34 @@ const claimEnergy = async (token, uid, amount, includes, type, id) => {
       },
       {
         headers: {
-          Authorization: token,
+          Accept: 'application/json, text/plain, */*',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Accept-Language': 'en-US,en;q=0.7',
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          Priority: 'u=1, i',
+          Referer: 'https://www.mintchain.io/mint-forest',
+          'Sec-Ch-Ua':
+            '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+          'Sec-Ch-Ua-Mobile': '?0',
+          'Sec-Ch-Ua-Platform': '"macOS"',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Gpc': '1',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+          Authorization: token, // Authorization header with token
         },
       }
     );
-    console.log(response.data.msg);
+    return response.data.msg; // Return the message from the response
   } catch (error) {
-    throw error;
+    throw error; // Throw error if claiming fails
   }
 };
 
+// Function to inject energy to an address using the API
 const injectEnergy = async (token, energy, address) => {
   try {
     const response = await axios.post(
@@ -67,21 +123,40 @@ const injectEnergy = async (token, energy, address) => {
       },
       {
         headers: {
-          Authorization: token,
+          Accept: 'application/json, text/plain, */*',
+          'Accept-Encoding': 'gzip, deflate, br, zstd',
+          'Accept-Language': 'en-US,en;q=0.7',
+          'Cache-Control': 'no-cache',
+          Pragma: 'no-cache',
+          Priority: 'u=1, i',
+          Referer: 'https://www.mintchain.io/mint-forest',
+          'Sec-Ch-Ua':
+            '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+          'Sec-Ch-Ua-Mobile': '?0',
+          'Sec-Ch-Ua-Platform': '"macOS"',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Gpc': '1',
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+          Authorization: token, // Authorization header with token
         },
       }
     );
-    return response.data;
+    return response.data; // Return the response data
   } catch (error) {
-    throw error;
+    throw error; // Throw error if injection fails
   }
 };
 
+// Function to get the list of tokens from a JSON file
 const getTokenList = () => {
   const jsonFileContent = fs.readFileSync('accounts.json');
-  return JSON.parse(jsonFileContent);
+  return JSON.parse(jsonFileContent); // Parse and return JSON content
 };
 
+// Export the functions for external use
 module.exports = {
   fetchEnergy,
   fetchUserInfo,
